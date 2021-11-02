5938dadf39347b3eaabd8686def0608fc6ebbf9e
Liebestraum1 <psnew14@gmail.com>
2021-11-02 23:01:56 +0900

    week 7

--- app/src/main/java/com/example/apppro_5/MainActivity.java
+++ app/src/main/java/com/example/apppro_5/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.apppro_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bt_launch_onclick(View v) {
+        FrameLayout layout = findViewById(R.id.layout);
+
+        Button bt = new Button(this);
+
+        layout.addView(bt);
     }
 }
